71b05bcc756426536bacb4b363eaf970527d775d
nazmul162001 <nazmulprime162001@gmail.com>
2022-04-01 12:12:43 +0600

    added nav & 3 custom link

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div>
+      <h2>Wellcome to my bd store</h2>
+      <nav>
+        <CustomLink to='/home'>Home</CustomLink>
+        <CustomLink to='/orderreview'>Review</CustomLink>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
