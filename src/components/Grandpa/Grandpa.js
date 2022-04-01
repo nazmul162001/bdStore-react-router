1096d7f02b6fb1bfe938533c98552cf5d63edb18
nazmul162001 <nazmulprime162001@gmail.com>
2022-04-01 19:07:42 +0600

    sent data using contexApi

--- src/components/Grandpa/Grandpa.js
+++ src/components/Grandpa/Grandpa.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import './Grandpa.css';
 import Father from '../Father/Father';
 import Uncle from '../Uncle/Uncle';
 import Aunty from '../Aunty/Aunty';
 
+export const RingContext = createContext('diamond');
+const ring = 'Diamond come from ContexApi'
+
 const Grandpa = () => {
   const house = 7;
   return (
-    <div className="grandpa">
-      <h3>Grandpa</h3>
-      <div style={{display:'flex'}}>
-        <Father house={house}></Father>
-        <Uncle house={house}></Uncle>
-        <Aunty house={house}></Aunty>
+    <RingContext.Provider value={ring}>
+      <div className="grandpa">
+        <h3>Grandpa</h3>
+        <div style={{ display: 'flex' }}>
+          <Father house={house}></Father>
+          <Uncle house={house}></Uncle>
+          <Aunty house={house}></Aunty>
+        </div>
       </div>
-    </div>
+    </RingContext.Provider>
   );
 };
 
--- src/components/MySelf/MySelf.js
+++ src/components/MySelf/MySelf.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RingContext } from '../Grandpa/Grandpa';
 
 const MySelf = ({house}) => {
+  const ring = useContext(RingContext)
   return (
     <div>
       <h3>meee</h3>
-      <p><small>House: {house} </small></p>
+      <p><small>House: {ring} </small></p>
     </div>
   );
 };
